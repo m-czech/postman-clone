758233285245aa3706465099d5d2cd50403ba122
m-czech <felvi00@gmail.com>
2024-01-17 21:04:57 +0100

    Add url bar component

---
+++ src/renderer/src/components/RequestView.tsx
@@ -0,0 +1,5 @@
+import UrlBar from "./UrlBar"
+
+export default function RequestView() {
+    return <UrlBar></UrlBar>
+}
---
+++ src/renderer/src/components/UrlBar.tsx
@@ -0,0 +1,35 @@
+import Box from '@mui/joy/Box'
+import Dropdown from "@mui/joy/Dropdown";
+import Menu from '@mui/joy/Menu'
+import MenuItem from '@mui/joy/MenuItem'
+import MenuButton from '@mui/joy/MenuButton'
+import Input from "@mui/joy/Input";
+import { Divider } from "@mui/joy";
+
+
+export default function UrlBar() {
+    return (
+        <div>
+            <Box 
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'row'
+                }}
+            >
+                <Dropdown>
+                    <MenuButton variant='soft'>select method</MenuButton>
+                    <Menu >
+                        <MenuItem>GET</MenuItem>
+                        <MenuItem>POST</MenuItem>
+                    </Menu>
+                </Dropdown>
+                <Divider orientation="vertical"></Divider>
+                <Input
+                    variant="soft"
+                    placeholder="Enter URL"
+                >
+                </Input>
+            </Box>
+        </div>
+    )
+}
